26b955580e38f3c48d26d288484a40853b2435ad
0xasif.eth <52921890+rifatmax67@users.noreply.github.com>
2025-07-04 00:36:47 +0600

    Delete main.ts

--- main.ts
+++
@@ -1,7 +0,0 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="deno.ns" />
-import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-import config from "./fresh.config.ts";
-
-await start(manifest, config);
